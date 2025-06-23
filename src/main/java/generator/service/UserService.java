7f80d527fb6d96585f61bb6068931a8f060952ae
Albert Zhang <albert_zhang@berkeley.edu>
2025-06-23 14:10:16 +0800

    Remove .idea folder from repository

---
+++ src/main/java/generator/service/UserService.java
@@ -0,0 +1,13 @@
+package generator.service;
+
+import generator.domain.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author albertzhang
+* @description 针对表【user(用户表)】的数据库操作Service
+* @createDate 2025-06-23 13:56:37
+*/
+public interface UserService extends IService<User> {
+
+}
