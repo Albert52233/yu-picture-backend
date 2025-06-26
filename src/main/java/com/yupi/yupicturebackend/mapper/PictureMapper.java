488ce78211ca68eb96d0c8193c872082cf7262ef
Albert Zhang <albert_zhang@berkeley.edu>
2025-06-26 16:10:30 +0800

    picture module finished --STAGE--

---
+++ src/main/java/com/yupi/yupicturebackend/mapper/PictureMapper.java
@@ -0,0 +1,12 @@
+package com.yupi.yupicturebackend.mapper;
+
+import com.yupi.yupicturebackend.model.entity.Picture;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+
+public interface PictureMapper extends BaseMapper<Picture> {
+
+}
+
+
+
